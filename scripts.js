df7a9c6b85083d86e7a0f399de49005d8f2ef69f
Sadia131 <sadiaobaed6969@gmail.com>
2022-08-19 00:09:25 +0600

    html css done

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+document.getElementById('display').addEventListener('click', function(){
+    const displayName = document.getElementById('display');
+    const displayCount = parseFloat(displayName.value);
+    console.log(displayCount)
+
+})
+
+
+let buttons = document.getElementsByClassName('button');
